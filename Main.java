709621a626e19e6c048813a51d2cdfe33220b897
RidhoFahrizal <farizalridho@gmail.com>
2024-06-07 00:06:24 +0700

    learn-the-basic-2

--- Main.java
+++ Main.java
@@ -1,11 +1,27 @@
-public class Main {
 
+
+
+public class Main {
+    
     /* 
-     * 1. java hello.world
-     * 2. java variable 
-     * 3. java method sederhana
-     * 4. 
+    * 1. java hello.world
+    * 2. java variable 
+    * 3.0 java method sederhana
+    * 3.1 Java method parameter
+    * 3.2 Java method overloading 
+    * 3.3 Java scope
+    * 3.4 Java Rekursive Method
     */
+    
+    static int plusMethod(int x, int y){
+            return x+y;
+        }
+    
+    static double plusMethod(double x, double y){
+            return x+y;
+        }
+
+            
     public static void main(String[] args) {
         
         //1.java hello world 
@@ -28,7 +44,31 @@ public class Main {
 
         //3.method in java 
         myFirstMethod.myMethod();      
+
+        //3.1 method parameter
+        NamaLengkap.inputNamaLengkap("Ridho");        
+
+        //3.2 method overloading
+        int mynum1 = plusMethod(4, 5);
+        double mynum2 = plusMethod(7.2, 5.4);
+        System.out.println("INT:" + mynum1);
+        System.out.println("DOUBLE:" + mynum2);
+
+        //3.3
+        JavaScope.Scope();
+        
+        //3.4
+        System.out.println("Ini adalah rekursive hitung mundur:" );
+         countdown(3);
     }
+
+    public static void countdown(int num) {
+    if (num >= 0) {
+        System.out.println(num);
+        countdown(num - 1);
+    }
+}
+
 }
 
 class myFirstMethod{
@@ -36,4 +76,20 @@ class myFirstMethod{
         static void myMethod(){
             System.out.println("Hallo Ridho Fahrizal");
         }
+    } 
+
+class NamaLengkap{
+        static void inputNamaLengkap(String fname){
+               System.out.println("Hallo "+ fname+"pada baris ini kamu telah melakukan method parameter");
+        }
     }
+    
+    //3.3 Java scope
+class JavaScope{
+    static void Scope(){
+        // variable a tidak dapat di akses (tidak dikenal diatas sini)    
+        int a=3;
+            System.out.println("a");
+    }
+}
+
