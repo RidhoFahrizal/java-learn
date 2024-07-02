cfabde88a02244cb3b590d9ec361b87fe516c67f
RidhoFahrizal <farizalridho@gmail.com>
2024-07-02 22:10:25 +0700

    menambahkan folder oop

---
+++ 2.Java-OOP/PersonApp.java
@@ -0,0 +1,13 @@
+// Definisi kelas Person
+
+
+// Kelas utama untuk menjalankan program
+public class PersonApp {
+    public static void main(String[] args) {
+        // Membuat objek person1 dari kelas Person
+        var person1 = new Person("Alice", 30);
+
+        // Memanggil method dari objek person1
+        person1.sayHello();
+    }
+}
---
+++ 2.Java-OOP/person.java
@@ -0,0 +1,16 @@
+class Person {
+    // Field atau atribut
+    String name;
+    int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method atau fungsi
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
